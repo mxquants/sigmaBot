1ecae095f03b529f56f768b17f0d6fab977ebb53
rhdzmota <rhdzmota@mxquants.com>
2017-04-26 22:52:20 -0500

    get started button and first greeting message

--- get_started.sh
+++ get_started.sh
@@ -2,4 +2,4 @@ curl -X POST -H "Content-Type: application/json" -d '{
   "get_started":{
     "payload":"GET_STARTED_PAYLOAD"
   }
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAaiAN9H6KEBANNiZCZA1xnwt1wxd9twtZADhHkfvpWHCh8JaVd7CNZB61Yb0jMZA4KAFChAyDNz74ZCpoaWyvNGH2khu6N8LdVEzePFJZC6ueCfvM9Tm9R0d8Ebj4DoZC8CTNbrVISI3DB0MMZBFtNQRlvH9WRcc2xfhS1tCTNJyOQZDZD"
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAADMZBDhEKOoBAOZCgvKHj6tlf8lQTMNB6MUvPTQQEgluJCXt4VZAH7zTx25NnPD25TOp1ZAG5p0ayksyfjGUMrmeIfVy5tqQq7yMKM27LWx1nfZBVEpn4zQfLXrIzo8FC5RmKywQumyswxp1dDcjqvyFqaNoKFnuq0ucfvGv6QZDZD"
--- greeting.sh
+++ greeting.sh
@@ -2,7 +2,7 @@ curl -X POST -H "Content-Type: application/json" -d '{
   "greeting":[
     {
       "locale":"default",
-      "text":"Hi there!\n\nMy name is pyBot, the only bot that can talk with snakes! Use #py at the beginning of your code so I can speak Parseltongue with you!"
+      "text":"Hi there!\n\nMy name is sigma, your personal financial assistant!"
     }
   ] 
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAaiAN9H6KEBANNiZCZA1xnwt1wxd9twtZADhHkfvpWHCh8JaVd7CNZB61Yb0jMZA4KAFChAyDNz74ZCpoaWyvNGH2khu6N8LdVEzePFJZC6ueCfvM9Tm9R0d8Ebj4DoZC8CTNbrVISI3DB0MMZBFtNQRlvH9WRcc2xfhS1tCTNJyOQZDZD"
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAADMZBDhEKOoBAOZCgvKHj6tlf8lQTMNB6MUvPTQQEgluJCXt4VZAH7zTx25NnPD25TOp1ZAG5p0ayksyfjGUMrmeIfVy5tqQq7yMKM27LWx1nfZBVEpn4zQfLXrIzo8FC5RmKywQumyswxp1dDcjqvyFqaNoKFnuq0ucfvGv6QZDZD"
